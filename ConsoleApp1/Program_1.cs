13d2571ecafca549d241219952c3ccd447d31403
sasakozukhar <sasakozuhar93@gmail.com>
2024-11-04 16:59:22 +0200

    Create Program_1.cs

---
+++ ConsoleApp1/Program_1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task_1
+            //Console.WriteLine("Введите первое число:");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите второе число:");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+
+            //double sum = number1 + number2;
+
+            //Console.WriteLine("Сумма чисел: " + sum);
+
+            //Console.ReadKey();
+
+            //task_2
+            //Console.WriteLine("Введите первое число:");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите второе число:");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+
+            //double sum = number1 + number2;
+
+            //Console.WriteLine($"{number1} + {number2} = {number2} + {number1}");
+
+            //Console.ReadKey();
+
+            //task_3
+            //Console.WriteLine("Введите первое число:");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите второе число:");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+
+            //double product = number1 * number2;
+
+            //Console.WriteLine($"{number1} * {number2} = {product}");
+
+            //Console.ReadKey();
+
+            //task_4
+            //Console.WriteLine("Введите первое число:");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите второе число:");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите третье число:");
+            //double number3 = Convert.ToDouble(Console.ReadLine());
+
+            //double result = number1 * number2 + number3;
+
+            //Console.WriteLine($"{number1:F2} * ({number2:F2} + {number3:F2}) = ({number3:F2} * {number2:F2}) + {number1:F2}");
+
+            //Console.ReadKey();
+
+            //task_5
+            //     Console.WriteLine("Введите первое число:");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите второе число:");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите третье число:");
+            //double number3 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите четвертое число:");
+            //double number4 = Convert.ToDouble(Console.ReadLine());
+
+            //double result = (number1 / number2) + (number3 / number4);
+
+            //Console.WriteLine($"{number1:F2} / {number2:F2} + {number3:F2} / {number4:F2} = {result:F2}");
+
+            //Console.ReadKey();
+
+            //task_6
+            //Console.WriteLine("Введите первое число:");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите второе число:");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+
+            //double sum = number1 + number2;
+            //double difference = number1 - number2;
+            //double product = number1 * number2;
+
+            //Console.WriteLine($"{number1:F2} + {number2:F2} = {sum:F2}");
+            //Console.WriteLine($"{number1:F2} - {number2:F2} = {difference:F2}");
+            //Console.WriteLine($"{number1:F2} * {number2:F2} = {product:F2}");
+            //Console.ReadKey();
+
+            //task_7
+            //Console.WriteLine("Введите первое число (a):");
+            //double a = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите второе число (b):");
+            //double b = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите третье число (c):");
+            //double c = Convert.ToDouble(Console.ReadLine());
+
+            //double result1 = a * (b + c);
+            //double result2 = (b + c) * a;
+
+            //Console.WriteLine("[a * (b + c)] = [(b + c) * a]");
+            //Console.WriteLine($"[{a:F2} * ({b:F2} + {c:F2})] = [{(b + c):F2} * {a:F2}]");
+            //Console.WriteLine($"{result1:F2} = {result2:F2}");
+
+            //Console.ReadKey();
+
+            //task_8        
+            //Console.WriteLine("Введите первое число (a):");
+            //double a = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите второе число (b):");
+            //double b = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите третье число (c):");
+            //double c = Convert.ToDouble(Console.ReadLine());
+
+            //double result1 = a + (b + c);
+            //double result2 = (a + c) + b;
+
+            //Console.WriteLine($"({a:F4} + ({b:F4} + {c:F4})) = ({a:F4} + {c:F4} + {b:F4})");
+            //Console.WriteLine($"{result1:F4} = {result2:F4}");
+
+            //Console.ReadKey();
+
+            //task_9
+            //Console.WriteLine("Введите вещественное число (x):");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите целое число (y):");
+            //int y = Convert.ToInt32(Console.ReadLine());
+
+            //double result = (3 * x) / y;
+
+            //Console.WriteLine($"({x:F4} * 3) / {y} = {result:F4}");
+            //Console.WriteLine(  $"Округлённый с точностью до 4 знаков {result:F4} "); 
+            //Console.ReadKey();
+
+            //task_10
+            //Console.WriteLine("Введите первое вещественное число:");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите вещественное второе число:");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+
+            //double product = number1 * number2;
+
+            //Console.WriteLine($"{number1:F2} * {number2:F2} = {product:F3} | {product:F5}");
+
+            //Console.ReadKey();
+        }
+    }
+}
